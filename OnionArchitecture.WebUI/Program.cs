72e28f1ae8407dde1f0862197c24e28f454b6e84
huseyinekh <huseyinekh@CANR2-4>
2023-02-17 13:15:22 +0400

    update WebUI

--- OnionArchitecture.WebUI/Program.cs
+++ OnionArchitecture.WebUI/Program.cs
@@ -1,7 +1,15 @@
+
+
+using Microsoft.EntityFrameworkCore;
+using OnionArchitecture.Repo;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(opt=>
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("default")));
+
 
 var app = builder.Build();
 
